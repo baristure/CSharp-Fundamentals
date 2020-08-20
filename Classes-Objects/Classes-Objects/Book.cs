b9b7db4521cf50fa0eccf78c0c8400099003b6da
Baris Ture <baristure@hotmail.com>
2020-08-20 16:34:03 +0300

    Classes & Objects

---
+++ Classes-Objects/Classes-Objects/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Objects
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+        public double rating;
+        public Book()
+        {
+
+        }
+        public Book(string aTitle, string aAuthor, int aPages, double aRating)
+        {
+            title =aTitle;
+            author = aAuthor;
+            pages = aPages;
+            rating = aRating;
+        }
+
+        public bool BookAwards()
+        {
+            if(rating >= 8.0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Classes-Objects/Classes-Objects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter","JK Rowling",400,9.0);
+            Book book2 = new Book("Lord of The Rings", "Tolkien", 700,5.0);
+
+            book2.title = "The Hobbit";
+            Console.WriteLine(book1.BookAwards());
+            Console.WriteLine(book2.BookAwards());
+
+
+            Console.ReadLine();
+        }
+    }
+}
